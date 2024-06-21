7e7d029c6cfbe947a936579e1cffa536146aacd0
julius <nyerereoffice@gmail.com>
2024-06-21 20:31:09 +0300

    Header & Navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,36 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu')
+const navToggle = document.getElementById('nav-toggle')
+const navClose = document.getElementById('nav-close')
+
+// Show Menu
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// Hide Menu
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+
+    // When we click on each nav__link we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD SHADOW HEADER ===============*/
 
