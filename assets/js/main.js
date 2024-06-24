0b2f62ceaa9b1fc2cfd2449a305a43a690e2c331
julius <nyerereoffice@gmail.com>
2024-06-24 07:05:56 +0300

    Scroll Up

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,15 @@ window.addEventListener('scroll', shadowHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+
+    // when the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll up class
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
